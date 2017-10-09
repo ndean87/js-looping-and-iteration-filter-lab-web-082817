a45512188b44b3693ed11ea7cc2f94ac8580667c
ndean87 <ndean87@gmail.com>
2017-10-09 13:12:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file
+// // Code your solution in this file
+
+
+
+function findMatching(drivers_arr, name){
+  return drivers_arr.filter(function(n) { return n.toLowerCase() === name.toLowerCase() });
+}
+
+function fuzzyMatch(drivers_arr, letters){
+  let lengthOfName = letters.length
+  return drivers_arr.filter(function(n) { return n.slice(0, lengthOfName) === letters });
+}
+
+function matchName(drivers_arr, driver){
+  return drivers_arr.filter(function(n) { return n.name.toLowerCase() === driver.toLowerCase()})
+}
